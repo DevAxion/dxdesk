3f91ef9ea94af3ea9cf1d93be0616841ba39ee39
devaxion <user.ntds@gmail.com>
2026-09-17 14:37:30 +0400

    install-client.bat: log yazma, mənbə/kopyalama yoxlaması və aydın xəta mesajları

--- deploy/install-client.bat
+++ deploy/install-client.bat
@@ -1,25 +1,50 @@
 @echo off
+setlocal EnableExtensions
 REM ============================================================
-REM  MillaRemote Client - GPO Computer Startup skripti
-REM  SYSTEM kimi işləyir: faylı kopyalayır, firewall açır və
-REM  hər istifadəçi girişində başlaması üçün HKLM Run-a yazır.
+REM  MillaRemote Client - GPO Computer Startup skripti (loglu)
+REM  Log: %ProgramData%\MillaRemote\install.log
 REM ============================================================
 
 set "SRC=\milladc01\software_deploy\dist-client"
 set "DEST=%ProgramData%\MillaRemote"
+set "LOG=%DEST%\install.log"
 
 if not exist "%DEST%" mkdir "%DEST%"
+echo ================================================>> "%LOG%"
+echo [%date% %time%] install basladi>> "%LOG%"
+echo SRC=%SRC%>> "%LOG%"
+echo istifadeci: %USERNAME%  komputer: %COMPUTERNAME%>> "%LOG%"
 
-REM --- Faylları yalnız dəyişəndə kopyala ---
-robocopy "%SRC%" "%DEST%" MillaRemote.Client.exe appsettings.json /XO /R:2 /W:5 >nul
-
-REM --- Firewall: admin viewer-in qoşulması üçün 7000 (inbound) ---
-netsh advfirewall firewall show rule name="MillaRemote Stream 7000" >nul 2>&1
-if errorlevel 1 (
-  netsh advfirewall firewall add rule name="MillaRemote Stream 7000" dir=in action=allow protocol=TCP localport=7000 >nul
+REM --- Menbe elcatandirmi? ---
+if not exist "%SRC%\MillaRemote.Client.exe" (
+  echo XETA: menbe tapilmadi -^> %SRC%\MillaRemote.Client.exe>> "%LOG%"
+  echo XETA: "%SRC%" elcatan deyil. Paylasim icazeleri / sebeke.
+  echo Ətraflı: %LOG%
+  goto :end
 )
 
-REM --- Hər istifadəçi girişində avtomatik başlatma (HKLM Run) ---
-reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /v MillaRemote /t REG_SZ /d "\"%DEST%\MillaRemote.Client.exe\"" /f >nul
+REM --- Kopyala (loga yaz) ---
+robocopy "%SRC%" "%DEST%" MillaRemote.Client.exe appsettings.json /R:2 /W:3>> "%LOG%" 2>&1
+echo robocopy exit=%ERRORLEVEL%>> "%LOG%"
 
+REM --- Yoxla ---
+if not exist "%DEST%\MillaRemote.Client.exe" (
+  echo XETA: kopyalama alinmadi.>> "%LOG%"
+  echo XETA: fayl kopyalanmadi. Bax: %LOG%
+  goto :end
+)
+
+REM --- Firewall: 7000 inbound ---
+netsh advfirewall firewall show rule name="MillaRemote Stream 7000" >nul 2>&1
+if errorlevel 1 netsh advfirewall firewall add rule name="MillaRemote Stream 7000" dir=in action=allow protocol=TCP localport=7000>> "%LOG%" 2>&1
+
+REM --- Autostart (HKLM Run) ---
+reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /v MillaRemote /t REG_SZ /d "\"%DEST%\MillaRemote.Client.exe\"" /f>> "%LOG%" 2>&1
+
+echo [%date% %time%] UGURLU: quraşdırma tamamlandi.>> "%LOG%"
+echo UGURLU. Client %DEST% qovlugunda. Log: %LOG%
+
+:end
+echo [%date% %time%] bitdi>> "%LOG%"
+endlocal
 exit /b 0
